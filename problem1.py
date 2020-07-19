5b31028dc5709b9cb4264a58237be79cbd63f029
vvk1999 <vinaykshirsagar1999@gmail.com>
2020-07-19 19:26:03 +0530

    [T - 1] implementation

--- problem1.py
+++ problem1.py
@@ -5,33 +5,36 @@ f=open("data.json")
 data=json.load(f)
 
 
-
+cur='INR'
 start='2019-01-01'
 end='2019-01-31'
 
-print(start+"\n"+end)
+#print(start+"\n"+end)
 
 maxV=0.00
 minV=10000000
 
 for i in sorted(data['rates']):
-    val=float(data['rates'][i]['INR'])
+    val=float(data['rates'][i][cur])
     if val!=0.0:
         if start<=i and end>=i :
-            print(i+" "+str(val))
+            #print(i+" "+str(val))
             if minV > val:
                 minV=val
             if maxV < val:
                 maxV=val
 
-print(str(minV)+"\n"+str(maxV))
-minV=minV-0.1*minV
+#print(str(minV)+"\n"+str(maxV))
+minV=minV-0.05*minV
 rng=maxV-minV
 
 units=30
 
+print("graph of "+cur+" exchange rate against EUR from "+start+" to "+end)
+print("-"*100+">")
+
 for i in sorted(data['rates']):
     if start<=i and end>=i :
-        val=float(data['rates'][i]['INR'])
+        val=float(data['rates'][i][cur])
         if val != 0 :
-            print("="*(int(units)//int(val-minV)))
+            print(i+" |"+"▇"*int((val-minV)*10)+" "+str(val))
