002cb598ff660f20daa0fde24683c2a05ebac7ea
vvk1999 <vinaykshirsagar1999@gmail.com>
2020-07-19 12:42:04 +0530

    added min max and range

--- problem1.py
+++ problem1.py
@@ -11,8 +11,27 @@ end='2019-01-31'
 
 print(start+"\n"+end)
 
+maxV=0.00
+minV=10000000
+
 for i in sorted(data['rates']):
-    if start<=i and end>=i:
-        print(i)
-        print("="*int(data['rates'][i]['INR']))
-        print("\n")
+    val=float(data['rates'][i]['INR'])
+    if val!=0.0:
+        if start<=i and end>=i :
+            print(i+" "+str(val))
+            if minV > val:
+                minV=val
+            if maxV < val:
+                maxV=val
+
+print(str(minV)+"\n"+str(maxV))
+minV=minV-0.1*minV
+rng=maxV-minV
+
+units=30
+
+for i in sorted(data['rates']):
+    if start<=i and end>=i :
+        val=float(data['rates'][i]['INR'])
+        if val != 0 :
+            print("="*(int(units)//int(val-minV)))
